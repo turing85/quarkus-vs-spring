a69926813de714ab72d8ffb9a733fdc1c89333db
Marco Bungart <marco.bungart@googlemail.com>
2023-05-19 18:35:49 +0200

    Specify DB driver for flyway explicitly

--- spring/src/main/resources/db/migration/V2023_04_09__create_tables.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE public.animals (
-    id BIGINT CONSTRAINT animals__pk__id PRIMARY KEY,
-    name VARCHAR(63) NOT NULL,
-    species VARCHAR(63) NOT NULL,
-    CONSTRAINT animals__unique__name UNIQUE(name)
-);
-
-CREATE SEQUENCE animals__seq__id AS BIGINT START WITH 1 INCREMENT BY 50 OWNED BY public.animals.id;
